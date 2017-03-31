e614b7e020b9ce9dfe648a4ef64d1c965a3d7b3a
Dannae1 <obedmarcial94@gmail.com>
2017-03-31 05:39:32 -0600

    Tarea

---
+++ app/src/main/java/com/obedmarcial94gmail/formulario_obed1/confirmacion.java
@@ -0,0 +1,13 @@
+package com.obedmarcial94gmail.formulario_obed1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class confirmacion extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion);
+    }
+}
